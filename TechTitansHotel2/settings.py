1605fc0a4633d13417e8312e6c6b98e20826025c
Muhammad Bilal Ghazi <mbghazi@gmail.com>
2023-10-27 23:47:59 -0500

    minor updates

--- TechTitansHotel2/settings.py
+++ TechTitansHotel2/settings.py
@@ -75,10 +75,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'TechTitansHotel2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HotelMainDB',  # Replace with your actual database name
+        'USER': 'postgres',
+        'PASSWORD': '5722',
+        'HOST': 'localhost',  # Set to the address of your database
+        'PORT': '5432',  # Leave as is if using the default PostgreSQL port
     }
 }
 
