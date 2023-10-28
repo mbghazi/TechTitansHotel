2ee77fd6a013001539780f7c9d6d3bf9a9dbe010
Muhammad Bilal Ghazi <mbghazi@gmail.com>
2023-10-28 01:14:45 -0500

    changes 3

--- app/models.py
+++ app/models.py
@@ -5,3 +5,38 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    room_number = models.CharField(max_length=10, unique=True)
+    room_type = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    price_per_night = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'Room {self.room_number} - {self.room_type}'
+
+class Guest(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"Reservation for {self.guest} - Room {self.room}"
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
