b4de34fc30800d855369cf57e6219e7e87abea08
Muhammad Bilal Ghazi <mbghazi@gmail.com>
2023-11-01 00:28:59 -0500

    changes 7

--- TechTitansHotel2/urls.py
+++ TechTitansHotel2/urls.py
@@ -9,23 +9,23 @@ from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 
 urlpatterns = [
+    # Application-specific URLs
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+    path('rooms/', views.room_list, name='room_list'),
+    path('rooms/<int:room_id>/', views.room_detail, name='room_detail'),
+
+    # Authentication-related URLs
     path('login/',
-         LoginView.as_view
-         (
+         LoginView.as_view(
              template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
+             extra_context={'title': 'Log in'}
          ),
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+
+    # Admin URL
     path('admin/', admin.site.urls),
-    path('rooms/', views.room_list, name='room_list'),
-    path('rooms/<int:room_id>/', views.room_detail, name='room_detail'),
 ]
--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Room, Guest, Reservation, Service
 
-admin.site.register(Room)
-admin.site.register(Guest)
-admin.site.register(Reservation)
-admin.site.register(Service)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('room_number', 'room_type', 'price_per_night', 'is_available')
+    search_fields = ('room_number', 'room_type')
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('guest', 'room', 'check_in', 'check_out')
+    search_fields = ('guest__first_name', 'guest__last_name', 'room__room_number')
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Guest, GuestAdmin)
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Service, ServiceAdmin)
