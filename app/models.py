969caf8bd943ca961edadb2aa56b10592698cc19
Muhammad Bilal Ghazi <mbghazi@gmail.com>
2023-11-18 20:16:59 -0600

    changes 13

--- app/models.py
+++ app/models.py
@@ -34,6 +34,14 @@ class Reservation(models.Model):
     def __str__(self):
         return f"Reservation for {self.guest} - Room {self.room}"
 
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class RoomService(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
